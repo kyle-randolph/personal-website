252b0d3396ba41a080700a979b0366cd826fa2ce
kyle-randolph <randolphkylea@gmail.com>
2023-06-26 19:38:12 -0600

    setting app to run on default http port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/contact', (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = 80;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
