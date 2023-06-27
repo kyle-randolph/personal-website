7a766cd868baf821d6d429167dc9c8f95ec94e33
kyle-randolph <randolphkylea@gmail.com>
2023-06-26 19:30:55 -0600

    adding placeholder pages

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set up routes
+app.get('/', (req, res) => {
+  res.render('index', { pageTitle: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { pageTitle: 'About' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { pageTitle: 'Contact' });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
