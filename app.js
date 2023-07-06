c8f1ec139479e3ae595ca90c1c61a269a0fdf1fa
kyle-randolph <randolphkylea@gmail.com>
2023-07-05 21:13:32 -0600

    adding resume, updating formatting, adding favicon

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+countHome = 0;
+countAbout = 0;
+countContact = 0;
+countProjects = 0;
+countResume = 0;
+
+
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
@@ -10,15 +17,39 @@ app.use(express.static('views'));
 // Set up routes
 app.get('/', (req, res) => {
   res.render('index', { pageTitle: 'Home' });
+  console.log(req.get('User-Agent'));
+  countHome += 1;
+  console.log("Home page visits: ", countHome);
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { pageTitle: 'About' });
+  countAbout += 1;
+  console.log("About page visits: ", countAbout);
 });
 
 app.get('/contact', (req, res) => {
   res.render('contact', { pageTitle: 'Contact' });
+  countContact += 1;
+  console.log("Contact page visits: ", countContact);
 });
+
+app.get('/projects', (req, res) => {
+  res.render('projects', { pageTitle: 'Projects' });
+  countProjects += 1;
+  console.log("Projects page visits: ", countProjects);
+});
+
+app.get('/resume', (req, res) => {
+  res.render('resume', { pageTitle: 'Resume' });
+  countResume += 1;
+  console.log("Resume page visits: ", countResume);
+});
+
+app.get('/site.webmanifest', (req, res) => {
+  res.render('webmanifest', { pageTitle: 'webmanifest' });
+});
+
 
 // Start the server
 const defined_port = process.env.PORT || 80;
--- views/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.navbar-toggle').click(function() {
-      $('.navbar-menu').toggleClass('active');
-    });
-  });
-  
